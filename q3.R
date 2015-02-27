b4dcc54239a76c43369504397c663c39ab7d5b7e
Shuo Han <shuo_han@outlook.com>
2015-02-27 16:05:22 -0500

    modified q3.R

--- q3.R
+++ q3.R
@@ -1,13 +1,25 @@
-# generate 1000 random variables
 num = 1000
+
+# normal distribution
 rvs = rnorm(num, mean = 0, sd = 1)
 pvalues = 1 - pnorm(rvs, mean = 0, sd = 1) 
-normHist = hist(pvalues);
+normHist = hist(pvalues, 
+    main="histogram of P-values for the standard normal distribution", 
+    plot=FALSE)
 normHist$counts = normHist$counts / num
-plot(normHist)
+pdf('normal.pdf')
+plot(normHist, 
+    main="pdf plot of the P-values histogram for the standard normal distribution",
+    ylab="density")
 
+# beta distribution
 rvs = rbeta(num, 2, 5) 
 pvalues = 1 - pbeta(rvs, 2, 5)
-bHist = hist(pvalues);
+bHist = hist(pvalues, 
+    main="histogram of P-values for the beta distribution",
+    plot=FALSE)
 bHist$counts = bHist$counts / num
-plot(bHist)
+pdf('beta.pdf')
+plot(bHist, 
+    main="pdf plot of the P-values histogram for the beta distribution", 
+    ylab="density")
